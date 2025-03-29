78950dfaa583442078ce88fc8670d59e0c7be082
YanDSilva <yandsilva94@gmail.com>
2025-03-29 20:57:39 -0300

    feat: salvando

--- frontend/src/context/authcontext.jsx
+++ frontend/src/context/authcontext.jsx
@@ -1,62 +1,64 @@
-import { createContext, useState } from 'react';
-import apiLocal from '../Api/apiLocal';
-
+import { createContext, useState } from "react";
+import apiLocal from "../Api/apiLocal";
 
 export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
+  const [tokenT, setTokenT] = useState(false);
+  const [token, setToken] = useState("");
 
-    const [tokenT, setTokenT] = useState(false);
-    const [token, setToken] = useState("");
+  const authenticated = !!tokenT;
 
-    const authenticated = !!tokenT
-
-    async function verificaToken() {
-        const iToken = localStorage.getItem('@token');
-        if (!iToken) {
-            setTokenT(false);
-            return
-        }
-        const tokenU = JSON.parse(iToken);
-        setToken(tokenU);
-
-        try {
-            const resposta = await apiLocal.get('/verificaToken', {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            if (resposta.data.id) {
-                setTokenT(true);
-                localStorage.setItem('@id', JSON.stringify(resposta.data.id));
-                localStorage.setItem('@nome', JSON.stringify(resposta.data.nome));
-            }
-        } catch (error) {
-
-        }
+  async function verificaToken() {
+    const iToken = localStorage.getItem("@token");
+    if (!iToken) {
+      setTokenT(false);
+      return;
     }
+    const tokenU = JSON.parse(iToken);
+    setToken(tokenU);
 
-    async function login(email, password) {
-        try {
-            const resposta = await apiLocal.post('/Login', {
-                email,
-                password
-            });
-
-
-            localStorage.setItem('token', JSON.stringify(resposta.data.token));
-            localStorage.setItem('@id', JSON.stringify(resposta.data.id));
-            localStorage.setItem('@nome', JSON.stringify(resposta.data.nome));
-            setTokenT(true)
-
-        } catch (error) {
-
-        }
+    try {
+      const resposta = await apiLocal.get("/verificaToken", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (resposta.data.id) {
+        setTokenT(true);
+        localStorage.setItem("@id", JSON.stringify(resposta.data.id));
+        localStorage.setItem("@nome", JSON.stringify(resposta.data.nome));
+      }
+    } catch (error) {
+      console.log(error);
+      setTokenT(false);
+      localStorage.removeItem("@token");
+      localStorage.removeItem("@id");
+      localStorage.removeItem("@nome");
     }
+  }
 
-    return (
-        <AuthContext.Provider value={{ authenticated, login, verificaToken, token }}>
-            {children}
-        </AuthContext.Provider>
-    )
+  async function login(email, password) {
+    try {
+      const resposta = await apiLocal.post("/Login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", JSON.stringify(resposta.data.token));
+      localStorage.setItem("@id", JSON.stringify(resposta.data.id));
+      localStorage.setItem("@nome", JSON.stringify(resposta.data.nome));
+      setTokenT(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ authenticated, login, verificaToken, token }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
